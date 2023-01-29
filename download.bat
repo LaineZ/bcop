b45fc7e6b516c0050590489dac679c6220a76ce2
140bpmdubstep <laineprikol@gmail.com>
2023-01-29 13:05:48 +0300

    Rewrite to use powershell

--- download.bat
+++
@@ -1,7 +0,0 @@
-set FILE=sciter-js-sdk-4.4.9.3.zip
-
-if not exist %FILE% (
-    curl https://gitlab.com/sciter-engine/sciter-js-sdk/-/archive/4.4.9.3/sciter-js-sdk-4.4.9.3.zip --output %FILE%
-)
-
-powershell Expand-Archive %FILE% -DestinationPath .
