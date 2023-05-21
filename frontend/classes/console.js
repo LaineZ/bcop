b6a07be9fe66ee9188670fd6f296456ed84e3b45
140bpmdubstep <laineprikol@gmail.com>
2023-05-21 11:16:59 +0300

    Console improvemnets

--- frontend/classes/console.js
+++ frontend/classes/console.js
@@ -32,7 +32,13 @@ class Console {
     #domCommand(command, name, action) {
         if (command[0].toLowerCase() == name) {
 
-            if (!this.selectedDomElement) {
+            let element = this.selectedDomElement;
+
+            if (command.length > 1) {
+                element = $(command[1]);
+            }
+
+            if (!element) {
                 this.print("DOM element is missing!");
                 return;
             }
@@ -47,32 +53,39 @@ class Console {
     }
 
     executeCommand(input) {
-        let command = input.split(" ");
-        this.#domCommand(command, "remove", () => {
-            $(this.selectedDomElement).remove();
-        });
-
-        this.#domCommand(command, "text", () => {
-            $(this.selectedDomElement).text(command[1]);
-        });
-
-        this.#domCommand(command, "disable", () => {
-            $(this.selectedDomElement).attr("disabled", true);
-        });
-
-        this.#domCommand(command, "enable", () => {
-            $(this.selectedDomElement).attr("disabled", false);
-        });
-
-        this.#domCommand(command, "box", () => {
-            $(this.selectedDomElement).css("border", "1px solid red");
-            $(this.selectedDomElement).find("*").css("border", "1px solid red");
-        });
-
-        this.#domCommand(command, "rbox", () => {
-            $(this.selectedDomElement).css("border", "none");
-            $(this.selectedDomElement).find("*").css("border", "none");
-        });
+        try {
+            let result = eval(input);
+            this.print(result);
+        } catch (error) {
+            let command = input.split(" ");
+            this.print("> " + input);
+            this.#domCommand(command, "remove", () => {
+                $(this.selectedDomElement).remove();
+            });
+    
+            this.#domCommand(command, "text", () => {
+                $(this.selectedDomElement).text(command[1]);
+            });
+    
+            this.#domCommand(command, "disable", () => {
+                $(this.selectedDomElement).attr("disabled", true);
+            });
+    
+            this.#domCommand(command, "enable", () => {
+                $(this.selectedDomElement).attr("disabled", false);
+            });
+    
+            this.#domCommand(command, "box", () => {
+                $(this.selectedDomElement).css("border", "1px solid red");
+                $(this.selectedDomElement).find("*").css("border", "1px solid red");
+            });
+    
+            this.#domCommand(command, "rbox", () => {
+                $(this.selectedDomElement).css("border", "none");
+                $(this.selectedDomElement).find("*").css("border", "none");
+            });
+            this.print(error);
+        }
     }
 
     hide() {
